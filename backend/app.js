8a13ac853aa211b113638e0b6fc0a1884ae76470
Bil <bildadsimiyu6@gmail.com>
2024-09-21 23:17:44 +0300

    connect server to database

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT | 5000;
 const authRoute = require('./routes/authRoute');
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODBURI)
+.then(() => console.log(`Database connected: ${mongoose.connection.host}`))
+.catch((error) => console.log(error.message));
 
 //END OF IMPORTS  
 app.use(express.urlencoded({extended: true}));
