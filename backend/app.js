766e6aba8db576ed3e651b567aa310c3d341ed52
Bil <bildadsimiyu6@gmail.com>
2024-09-21 22:31:56 +0300

    set-up authRoute

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT | 5000;
+const authRoute = require('./routes/authRoute');
 
-//END OF IMPORTS   
-app.get('/', (req, res) => {
-    res.send("Hello World");
-}) 
+//END OF IMPORTS  
+app.use(express.urlencoded({extended: true}));
+app.use(express.json()); 
+app.use("/auth", authRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`)
