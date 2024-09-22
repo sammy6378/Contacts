5feca62ce1e00007e282bf0907ba18f504952d0c
Bil <bildadsimiyu6@gmail.com>
2024-09-22 16:27:41 +0300

    setup cloudinary config

--- backend/routes/contactRoute.js
+++ backend/routes/contactRoute.js
@@ -5,6 +5,11 @@ const authMiddleware = require("../middleware/authMiddleware");
 const cloudinary = require('cloudinary').v2;
 
 //cloudinary config
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 //?GET contacts/list
 route.get("/list", authMiddleware, async (req, res) => {
