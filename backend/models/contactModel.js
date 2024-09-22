dc7c28d783f84bf73c6aef077f6d936b976920be
Bil <bildadsimiyu6@gmail.com>
2024-09-22 14:38:42 +0300

    add get and add routes

--- backend/models/contactModel.js
+++ backend/models/contactModel.js
@@ -2,7 +2,34 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const contactSchema = new Schema({
-
+    image: {
+        type: String,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    number: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
 })
 
 const contactModel = mongoose.model('contacts', contactSchema);
--- backend/routes/contactRoute.js
+++ backend/routes/contactRoute.js
@@ -1,6 +1,53 @@
 const express = require('express');
-const contactModel = require('../models/contactModel');
+const contactsModel = require('../models/contactModel');
 const route = express.Router();
+const authMiddleware = require('../middleware/authMiddleware');
+
+//?GET contacts/list
+route.get('/list', authMiddleware, async(req, res) => {
+    try {
+        const userId = req.userId;
+        const data = await contactsModel.find({userId});
+        if(!userId) {
+            return res.json({success: false, message: "User does not exist"})
+        }
+
+        if(!contacts) {
+            return res.json({success: false, message: "Contact List empty"})
+        }
+
+        res.json({success: true, data})
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+//?POST contacts/add
+route.post('/add', authMiddleware, async(req, res) => {
+    try {
+        const {name, email, number, address} = req.body;
+        if(name.trim() === "" || email.trim() === "" || address.trim() === "") {
+            return res.json({success: false, message: "Check for empty input fields"});
+        }
+
+        if(number.length !== 9) {
+            res.json({success: false, message: 'Number should be 9 digits long'});
+        }
+
+        const fullNumber = "+254" + number;
+
+        const data = await contactsModel.create({
+            name,
+            email,
+            number: fullNumber,
+            address
+        });
+
+        res.json({success: true, data, message: "Contact added succesfully"});
+    } catch (error) {
+        console.log(error);
+    }
+})
 
 
 module.exports = route;
