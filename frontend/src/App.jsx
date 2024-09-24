667489f2fdf244ffabf7148f55c5b19d85a56723
Bil <bildadsimiyu6@gmail.com>
2024-09-24 18:22:39 +0300

    import react-router-dom & style navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
+import { Route, Routes } from "react-router-dom"
 import Navbar from "./Components/Navbar"
+import Home from "./Pages/Home"
 
  
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </div>
   )
 }
--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,8 +1,44 @@
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div>
+      <nav className="bg-blue-500 text-white p-2">
+        <div className="flex justify-between items-center max-w-4xl mx-auto">
+        <Link to={'/home'}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-6"
+          >
+            <path
+              fillRule="evenodd"
+              d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </Link>
 
-export default Navbar
+        <button className="flex items-center uppercase border-2 border-red-500 p-0.5 rounded-md text-sm ">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-6"
+          >
+            <path
+              fillRule="evenodd"
+              d="M16.5 3.75a1.5 1.5 0 0 1 1.5 1.5v13.5a1.5 1.5 0 0 1-1.5 1.5h-6a1.5 1.5 0 0 1-1.5-1.5V15a.75.75 0 0 0-1.5 0v3.75a3 3 0 0 0 3 3h6a3 3 0 0 0 3-3V5.25a3 3 0 0 0-3-3h-6a3 3 0 0 0-3 3V9A.75.75 0 1 0 9 9V5.25a1.5 1.5 0 0 1 1.5-1.5h6ZM5.78 8.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 0 0 0 1.06l3 3a.75.75 0 0 0 1.06-1.06l-1.72-1.72H15a.75.75 0 0 0 0-1.5H4.06l1.72-1.72a.75.75 0 0 0 0-1.06Z"
+              clipRule="evenodd"
+            />
+          </svg>
+          Logout
+        </button>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
