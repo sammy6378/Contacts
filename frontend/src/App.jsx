739309c88d61bb1fdb261798b96e097f02e41f64
Bil <bildadsimiyu6@gmail.com>
2024-09-24 22:54:54 +0300

    fix routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,13 +6,17 @@ import Auth from "./Pages/Auth";
 const App = () => {
   return (
     <div>
-      <Navbar />
       <Routes>
         <Route path="/login" element={<Auth />} />
-        <Route path="/*">
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Auth />} />
-        </Route>
+        <Route
+          path="/*"
+          element={
+            <>
+              <Navbar />
+              <Route path="/" element={<Home />} />
+            </>
+          }
+        ></Route>
       </Routes>
     </div>
   );
