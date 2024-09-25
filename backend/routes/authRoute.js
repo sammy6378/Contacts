7df2e2d61291be4ea130db8ddfa32b88e171d244
Bil <bildadsimiyu6@gmail.com>
2024-09-25 09:18:24 +0300

    remove unnecessary comments

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -5,36 +5,6 @@ const validator = require('validator');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-
-/* route.post('/login', async(req, res) => {
-    try {
-        const {username, password} = req.body;
-        const user = await authModel.findOne({username});
-        if(username.trim() === "") {
-            return res.json({success: false, message: "username field empty"})
-        }
-        if(password.trim() === "") {
-            return res.json({success: false, message: "password field empty"})
-        }
-
-        if(!user) {
-            return res.json({success: false, message: "User not found"})
-        }
-
-        const isValidPassword = bcrypt.compare(password, user.password);
-        if(!isValidPassword) {
-            return res.json({success: false, message: "Password not found"});
-        }
-
-        const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET);
-        res.json({success: true, message: "Login Successful", token})
-        
-    } catch (error) {
-        console.log(error);
-        res.json({success: false, message: "Error login' in"});
-    }
-}) */
-
 //? POST auth/login
 route.post('/login', async(req, res) => {
     try {
