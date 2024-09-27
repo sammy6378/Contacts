6260f2bbadbbd9e02459d5429c68c52731d0be3a
Bil <bildadsimiyu6@gmail.com>
2024-09-27 13:12:27 +0300

    configure vercel.json file and cors

--- api/app.js
+++ api/app.js
@@ -1,26 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT | 5000;
-const cors = require('cors')
-const authRoute = require('./routes/authRoute');
-const contactRoute = require('./routes/contactRoute')
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODBURI)
-.then(() => console.log(`Database connected: ${mongoose.connection.host}`))
-.catch((error) => console.log(error.message));
-const allowedOrigins = ['https://contacts-client-vert.vercel.app'];
-//END OF IMPORTS  
-/* app.use(cors({
+const PORT = process.env.PORT || 5000;
+const cors = require("cors");
+const authRoute = require("./routes/authRoute");
+const contactRoute = require("./routes/contactRoute");
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.MONGODBURI)
+  .then(() => console.log(`Database connected: ${mongoose.connection.host}`))
+  .catch((error) => console.log(error.message));
+const allowedOrigins = ["https://contacts-client-vert.vercel.app"];
+//END OF IMPORTS
+app.use(
+  cors({
     origin: allowedOrigins,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allowed HTTP methods
     credentials: true, // Include cookies and HTTP authentication
-})); */
-app.use(cors());
-app.use(express.urlencoded({extended: true}));
-app.use(express.json()); 
+  })
+);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use("/auth", authRoute);
-app.use('/contacts', contactRoute);
+app.use("/contacts", contactRoute);
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on http://localhost:${PORT}`)
-})
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
