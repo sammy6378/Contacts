b1fcde80687001949fc879a305cad8289695a0b0
Bil <bildadsimiyu6@gmail.com>
2024-09-27 11:55:04 +0300

    configure cors

--- api/app.js
+++ api/app.js
@@ -8,9 +8,13 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODBURI)
 .then(() => console.log(`Database connected: ${mongoose.connection.host}`))
 .catch((error) => console.log(error.message));
-
+const allowedOrigins = ['https://contacts-client-vert.vercel.app'];
 //END OF IMPORTS  
-app.use(cors());
+app.use(cors({
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
+    credentials: true, // Include cookies and HTTP authentication
+}));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json()); 
 app.use("/auth", authRoute);
