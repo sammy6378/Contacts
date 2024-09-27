f46f4af01a3ecc085497b894f985da4de5112fcd
Bil <bildadsimiyu6@gmail.com>
2024-09-27 13:20:58 +0300

    add message for home page

--- api/app.js
+++ api/app.js
@@ -24,6 +24,10 @@ app.use(express.json());
 app.use("/auth", authRoute);
 app.use("/contacts", contactRoute);
 
+app.get('/', (req, res) => {
+    res.json("Hello")
+})
+
 app.listen(PORT, () => {
   console.log(`Server is listening on http://localhost:${PORT}`);
 });
