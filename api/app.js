51652aee351e436a32d792d521683cfc4b6c340f
Bil <bildadsimiyu6@gmail.com>
2024-09-27 12:04:46 +0300

    reset cors

--- api/app.js
+++ api/app.js
@@ -10,11 +10,12 @@ mongoose.connect(process.env.MONGODBURI)
 .catch((error) => console.log(error.message));
 const allowedOrigins = ['https://contacts-client-vert.vercel.app'];
 //END OF IMPORTS  
-app.use(cors({
+/* app.use(cors({
     origin: allowedOrigins,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
     credentials: true, // Include cookies and HTTP authentication
-}));
+})); */
+app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json()); 
 app.use("/auth", authRoute);
