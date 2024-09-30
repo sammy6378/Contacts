6dff7ac417f49f1977b123d2707b7982a866586c
Bil <bildadsimiyu6@gmail.com>
2024-09-30 10:27:28 +0300

    WIP: debugging

--- api/index.js
+++ api/index.js
@@ -1,16 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const mongoose = require("mongoose");  
-const mongodbURI = process.env.MONGODBURI
-mongoose.connect(mongodbURI)
-.then(() => console.log(`DB connected`))
-.catch(error => console.log(error));
-
-if (!mongodbURI) {
-  console.error("Error: MongoDB URI is not defined in environment variables.");
-  process.exit(1); // Exit the application if no URI is provided
-}
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const PORT = process.env.PORT || 5000;
 // Load environment variables from .env file
@@ -19,8 +10,7 @@ dotenv.config();
 const authRoute = require("./routes/authRoute");
 const contactRoute = require("./routes/contactRoute");
 
-
-/* // Check if the MongoDB URI is defined
+// Check if the MongoDB URI is defined
 if (!process.env.MONGODBURI) {
   console.error("Error: MongoDB URI is not defined in environment variables.");
   process.exit(1); // Exit the application if no URI is provided
@@ -36,7 +26,7 @@ mongoose
   .catch((error) => {
     console.error("Error connecting to MongoDB:", error.message);
     process.exit(1); // Exit if there’s an error connecting to the database
-  }); */
+  });
 
 // Middleware
 app.use(cors()); // Allows all origins for testing purposes, you can restrict this later
@@ -49,12 +39,10 @@ app.use("/auth", authRoute);
 app.use("/contacts", contactRoute);
 
 // Test route
-app.get('/', (req, res) => {
-    res.json("Hello from the server!");
+app.get("/", (req, res) => {
+  res.json("Hello from the server!");
 });
 
-// Export the app for Vercel
-//module.exports = app;
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`)
-})
+  console.log(`Server is listening on port ${PORT}`);
+});
