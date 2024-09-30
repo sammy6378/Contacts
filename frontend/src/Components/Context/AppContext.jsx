669f2abd0379ec515556d8e7c5f6b41f0b5e72d5
sammy6378 <samuel202mwangi@gmail.com>
2024-09-30 10:50:40 +0300

    update appcontext.jsx

--- frontend/src/Components/Context/AppContext.jsx
+++ frontend/src/Components/Context/AppContext.jsx
@@ -1,34 +1,72 @@
+// import { createContext, useEffect, useState } from "react";
+// import PropTypes from 'prop-types'
+// import { useNavigate } from "react-router-dom";
+
+// export const AppContext = createContext();
+
+// const ProviderFunction = (props) => {
+//     const url = import.meta.env.VITE_API_URL;
+
+//     const [contacts, setContacts] = useState([])
+//     const [token, setToken] = useState(null);
+//     const navigate = useNavigate()
+
+//     useEffect(() => {
+//         const isTokenAvailable = localStorage.getItem('token');
+//         if(isTokenAvailable) {
+//             setToken(isTokenAvailable);
+//         }
+//         else {
+//             navigate('/auth');
+//         }
+//     }, [token])
+//     return(
+//         <AppContext.Provider value={{url, contacts, setContacts, token, setToken}}>
+//             {props.children}
+//         </AppContext.Provider>
+//     )
+// }
+
+// ProviderFunction.propTypes = {
+//     children: PropTypes.node.isRequired,
+// }
+
+// export default ProviderFunction;
+
 import { createContext, useEffect, useState } from "react";
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 
 const ProviderFunction = (props) => {
-    const url = import.meta.VITE_API_URL;
+    const url = import.meta.env.VITE_API_URL; 
 
-    const [contacts, setContacts] = useState([])
-    const [token, setToken] = useState(null);
-    const navigate = useNavigate()
+    const [contacts, setContacts] = useState([]);
+    const [token, setToken] = useState(localStorage.getItem('token')); // Initial token state
+
+    const navigate = useNavigate();
 
     useEffect(() => {
-        const isTokenAvailable = localStorage.getItem('token');
-        if(isTokenAvailable) {
-            setToken(isTokenAvailable);
+        if (!token) {
+            const isTokenAvailable = localStorage.getItem('token');
+            if (isTokenAvailable) {
+                setToken(isTokenAvailable);
+            } else {
+                navigate('/auth');
+            }
         }
-        else {
-            navigate('/auth');
-        }
-    }, [token, navigate])
-    return(
-        <AppContext.Provider value={{url, contacts, setContacts, token, setToken}}>
+    }, [token,navigate]); 
+
+    return (
+        <AppContext.Provider value={{ url, contacts, setContacts, token, setToken }}>
             {props.children}
         </AppContext.Provider>
-    )
+    );
 }
 
 ProviderFunction.propTypes = {
     children: PropTypes.node.isRequired,
-}
+};
 
 export default ProviderFunction;
